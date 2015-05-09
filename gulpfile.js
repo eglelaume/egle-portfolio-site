5620849d965520a61831b1d3e9e288f40ebb55a5
eleventigers <phonodelik@gmail.com>
2015-05-09 15:47:11 +0100

    Resize images

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var rm = require('gulp-rimraf');
 var pngquant = require('imagemin-pngquant');
+var imageResize = require('gulp-image-resize');
+var parallel = require("concurrent-transform");
+var os = require("os");
 
-gulp.task('imagemin', function () {
+gulp.task('clean', function() {
+    return gulp.src('./images/*').pipe(rm());
+});
+
+gulp.task('copy', ['clean'], function() {
     return gulp.src('./artwork/**/*')
+        .pipe(gulp.dest('./images'));
+});
+
+gulp.task('imageresize', ['copy'], function () {
+    return gulp.src('./images/**/*.{jpg,png}')
+        .pipe(parallel(
+            imageResize({ width : 2880 }),
+            os.cpus().length
+        ))
+        .pipe(gulp.dest('./images'));
+});
+
+gulp.task('imagemin', ['imageresize'], function () {
+    return gulp.src('./images/**/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
@@ -12,5 +34,5 @@ gulp.task('imagemin', function () {
         .pipe(gulp.dest('./images'));
 });
 
-gulp.task('build', ['imagemin']);
+gulp.task('build', ['clean', 'copy', 'imageresize', 'imagemin']);
 gulp.task('default', ['build']);
