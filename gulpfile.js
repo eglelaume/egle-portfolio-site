fc972b67a7acfea98dcb7ce6f6b1a8d9bf934883
eglelaume <eglelaume@Ebonys-MBP.home.gateway>
2016-09-13 23:32:08 -0700

    WIP. Very important changes re creating pager. A lot of content refactoring done.

--- gulpfile.js
+++ gulpfile.js
@@ -36,3 +36,16 @@ gulp.task('imagemin', ['imageresize'], function () {
 
 gulp.task('build', ['clean', 'copy', 'imageresize', 'imagemin']);
 gulp.task('default', ['build']);
+
+
+$(document).ready(function(){
+    $(".button a").click(function(){
+        $(".overlay").fadeToggle(200);
+       $(this).toggleClass('btn-open').toggleClass('btn-close');
+    });
+});
+$('.overlay').on('click', function(){
+    $(".overlay").fadeToggle(200);   
+    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+    open = false;
+});
